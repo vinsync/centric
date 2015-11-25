86665ae2e93bcc2c4799d1cf58aa4586ef68f76c
vinsync <ceo@vinsync.com>
2015-11-25 20:40:55 +0530

    pushbots

--- myNodeApp/index.js
+++ myNodeApp/index.js
@@ -1,8 +1,8 @@
 // API : https://pushbots.com/developer/api/1
 var pushbots = require('pushbots');
 var Pushbots = new pushbots.api({
-    id: '553dd237177959d44f8b4567',
-    secret: '56ba71b9fdad4d111c816a1b2f79c594'
+    id: '56502e33177959a40c8b4568',
+    secret: '1ce856328560152f4a22a4d42d2188bb'
 });
 Pushbots.setMessage("Hello from pushbots node module", 1);
 Pushbots.customFields({
